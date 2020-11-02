1dcb27c81aa338327d36a4cf415ab3cd27b95592
wuxiaolong03 <2822376402@qq.com>
2020-11-02 19:10:55 +0800

    This is my first submission of

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import  (
+	"DataCertPlatformone/db_mysql"
+	_ "DataCertPlatformone/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//连接数据库
+	db_mysql.Connect()
+	//静态资源文件映射
+	beego.SetStaticPath("/js","./static/js")
+	beego.SetStaticPath("/css","./static/css")
+	beego.SetStaticPath("/img","./static/img")
+	beego.Run()
+}
+
