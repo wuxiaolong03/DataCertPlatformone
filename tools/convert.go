af2941ad7bc2fee85b4c1a0e9a8a30540f5d0970
wuxiaolong03 <2822376402@qq.com>
2021-01-08 09:45:22 +0800

    what

--- tools/convert.go
+++ tools/convert.go
@@ -1,12 +1,15 @@
 package tools
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 /**
  *将一个int64转换为[]byte
  */
 func Int64ToByte(num int64) []byte{
 	//Buffer:增益
-
+	fmt.Println("hello word")
 
 }
